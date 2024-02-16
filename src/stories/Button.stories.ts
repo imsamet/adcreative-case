2a109fdecbaca0d7abfe18f6649e3ad0ee102f26
Samet Çetin <46278098+imsamet@users.noreply.github.com>
2024-02-16 10:51:49 +0300

    build fix

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -13,6 +13,18 @@ const meta = {
       control: 'text',
       description: 'Overwritten description',
     },
+    href: {
+      control: 'text',
+      description: 'Overwritten description',
+    },
+    tabIndex: {
+      control: 'number',
+      description: 'Overwritten description',
+    },
+    type: {
+      options: ['button', 'a', 'Click', 'CrossSquare', 'Down', 'Figma', 'Github', 'Linkedin', 'Whatsapp'],
+      control: { type: 'radio' },
+    },
     icon: {
       options: ['Airplane', 'CheckCircle', 'Click', 'CrossSquare', 'Down', 'Figma', 'Github', 'Linkedin', 'Whatsapp'],
       control: { type: 'radio' },
@@ -25,6 +37,9 @@ type Story = StoryObj<typeof meta>;
 
 export const Component: Story = {
   args: {
+    type: 'a',
+    href: '#',
+    tabIndex: 1,
     label: 'Button',
   },
 };
